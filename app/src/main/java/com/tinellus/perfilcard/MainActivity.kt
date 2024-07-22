8bfa79d86fc32bcde9c6139e20ee514b3bf59275
gil-fullstack <gilservicosweb@gmail.com>
2024-07-22 14:16:17 -0300

    update with portifolio Box

--- app/src/main/java/com/tinellus/perfilcard/MainActivity.kt
+++ app/src/main/java/com/tinellus/perfilcard/MainActivity.kt
@@ -11,30 +11,34 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tinellus.perfilcard.ui.theme.PerfilCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,7 +47,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PerfilCardTheme {
-                myShowCar()
+                myShowCard()
             }
         }
     }
@@ -51,7 +55,7 @@ class MainActivity : ComponentActivity() {
 
 @SuppressLint("ResourceAsColor")
 @Composable
-fun myShowCar(){
+fun myShowCard(){
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
@@ -82,6 +86,13 @@ fun myShowCar(){
                      thickness = 4.dp,
                      color = colorResource(id = R.color.dar_blue)
                  )
+                     Column(modifier = Modifier.padding(7.dp)) {
+                         Text(text = "Gilvan Pereira",
+                             fontSize = 35.sp,
+                             color = Color(0xFF7A5A9E),
+                             fontWeight = FontWeight.Bold
+                         )
+                     }
              }
 
         }
@@ -106,10 +117,40 @@ private fun ProfilePhoto(mySize: Dp, strokeColor: Color) {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PerfilCardTheme {
-        myShowCar()
+        myShowCard()
+    }
+}
+
+@Preview
+@Composable
+fun Content(){
+    val myItems = listOf("Teste1", "teste 2", "Testando...")
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .padding(14.dp)) {
+        Portfolios(myItems)
+    }
+}
+
+@Composable
+private fun Portfolios(myItems: List<String>) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(14.dp),
+        shape = RoundedCornerShape(7.dp),
+        border = BorderStroke(width = 5.dp, color = Color(0xFF7A5A9E))
+    ) {
+        LazyColumn(contentPadding = PaddingValues(28.dp)) {
+            items(myItems) {
+                Text(text = "item", fontSize = 28.sp)
+            }
+        }
     }
 }
